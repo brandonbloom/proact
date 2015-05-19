6a0ea2ecf17ba288ea761177e2765f7d89f1b246
Brandon Bloom <brandon@brandonbloom.name>
2015-05-18 22:12:02 -0700

    Move TODO example to own ns

--- src/proact/core.cljs
+++ src/proact/core.cljs
@@ -1,17 +1,8 @@
 (ns ^:figwheel-always proact.core
   (:require [proact.render :refer [render-root]]
-            [proact.html :as html]
-            [proact.html-util :refer [classes link-to]]))
+            [proact.examples.todo :as todo]))
 
 (enable-console-print!)
-
-(println "Edits to this text should show up in your developer console.")
-
-(def todos [{:text "OMG"
-             :completed? true}
-            {:text "blah blah"
-             :completed? false}])
-
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
@@ -19,67 +10,4 @@
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
 
-
-
-(defn todo-item [{{:keys [completed? editing?] :as todo} :data, :as widget}]
-  (html/li {"className" (classes {"completed" completed?
-                                  "editing" editing?})}
-    (html/div {"className" "view"}
-      (html/input {"className" "toggle"
-                   "type" "checkbox"
-                   ;XXX onChange
-                   "checked" completed?})
-      (html/label {} (:text todo)) ;XXX onDoubleClick
-      (html/button {"className" "destroy"})) ;XXX onClick
-    ;;XXX ref editField
-    (html/input {"className" "edit"
-                       ;XXX "value" this.state.editText
-                       ;XXX onBlur, onChange, onKeyDown
-                       })))
-
-(defn todo-footer [{{:keys [active completed showing]} :data :as widget}]
-  ;XXX use completed
-  (html/footer {"id" "footer"}
-    (html/span {"id" "todo-count"}
-      (html/strong {} (str active)) (str showing " left"))
-    (html/ul {"id" "filters"}
-      (html/li {} (link-to "#/" "All"))
-      (html/li {} (link-to "#/active" "Active"))
-      (html/li {} (link-to "#/completed" "Completed")))))
-
-(defn app [widget]
-  (let [showing :active
-        items (for [{:keys [id completed?] :as todo} todos
-                    :when (case showing
-                            :active (not completed?)
-                            :completed completed?
-                            true)]
-                {:template todo-item
-                 :key id
-                 ; onToggle, onDestroy, onEdit, editing, onSave, onCancel
-                 :data todo})
-        completed (count (filter :completed? todos))
-        active (- (count todos) completed)
-        footer {:template todo-footer
-                ;XXX onClearCompleted
-                :data {:active active
-                       :completed completed
-                       :showing showing}}
-        main (when (seq todos)
-               (html/section {"id" "main"}
-                 (html/input {"id" "toggle-all"
-                              "type" "checkbox"
-                              ;;XXX onChange, checked
-                              "checked" (zero? active)})
-                 (html/ul {"id" "todo-list"} items)))
-        input (html/input {"id" "new-todo"
-                           "placeholder" "What needs to be done?"
-                           "autofocus" true})] ;XXX onKeyDown
-    (html/div {}
-      (html/header {"id" "header"}
-        (html/h1 {} "todos")
-        input)
-      main
-      footer)))
-
-(render-root "todoapp" (app {}))
+(render-root "todoapp" (todo/app {}))
