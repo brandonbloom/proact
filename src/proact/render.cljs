68178133dc5ec067b92c1409cc7b636674c9226a
Brandon Bloom <brandon@brandonbloom.name>
2015-05-23 14:42:59 -0700

    WIP on patching

--- src/proact/render.cljs
+++ src/proact/render.cljs
@@ -14,35 +14,84 @@
           (seq? x) (concat (flat x) xs*)
           :else (cons x xs*))))))
 
-(def ^:dynamic *render*)
+(def ^:dynamic *parent*)
+(def ^:dynamic *index*)
 
 (declare render)
+
+(defn get-node []
+  (aget (.-childNodes *parent*) *index*))
+
+(defn remove-node []
+  ;;TODO track this
+  (.removeChild (get-node)))
+
+(defn replace-node [node]
+  ;;TODO track this
+  (.replaceChild (get-node) node))
+
+(defn move-mode [node]
+  ;;TODO track this
+  (.insertBefore *parent* node (get-node)))
 
 (defn create-text [text]
   (.createTextNode js/document text))
 
-(defn create-element [{:as widget}]
+(defn patch-text [before after]
+  (if (string? before)
+    (let [node (get-node)]
+      (.replaceData node 0 (.-length node) after)
+      node)
+    (replace-node (create-text after))))
+
+(defn create-element [widget]
   (let [el (.createElement js/document (:html/tag widget))]
     (doseq [[k v] (:html/attributes widget)]
       (aset el k v))
-    (doseq [child (-> widget :children flat)]
-      (render el child))
+    (binding [*parent* el
+              *index* 0]
+      (doseq [child (-> widget :children flat)]
+        (.appendChild el (render child))
+        (set! *index* (inc *index*))))
     el))
 
-(defn render [parent {tag :html/tag :keys [template text] :as widget}]
+(defn patch-element [before after]
+  (if (= (:html/tag before) (:html/tag after))
+    (do
+      ;;TODO patch attributes
+      ;;TODO draw each child, calling move-node for each
+      ;;TODO keep track of all moved & removed nodes
+      (let [nodes (.-childNodes *parent*)
+            n 10000] ;XXX after's number of dom children
+        (while (> (.-length nodes) n)
+          (remove-node))))
+    (replace-node (create-element after))))
+
+(defn put [widget create patch]
+  (if-let [existing nil] ;TODO lookup by id & check not-moved
+    (patch existing widget) ;XXX use *index*, do insertBefore, handle moves
+    (create widget)))
+
+(defn put-text [text]
+  (put text create-text patch-text))
+
+(defn put-element [widget]
+  (put widget create-element patch-element))
+
+(defn render [{tag :html/tag :keys [template text] :as widget}]
   (cond
     (nil? widget) nil
-    (string? widget) (.appendChild parent (create-text widget))
-    template (recur parent (template widget))
-    text (recur parent (-> widget
-                         (dissoc :text)
-                         (assoc :children [(str text)])))
-    tag (.appendChild parent (create-element widget))
+    (string? widget) (put-text widget)
+    template (recur (template widget))
+    text (recur (-> widget (dissoc :text) (assoc :children [(str text)])))
+    tag (put-element widget)
     :else (assert false (str "TODO: just recurse?" widget))))
 
 (defn render-root [id widget]
-  (binding [*render* :TODO]
-    (let [el (.getElementById js/document id)]
-      (while (pos? (alength (.-children el)))
-        (.removeChild el (.-firstChild el)))
-      (render el widget))))
+  (binding [*parent* (.getElementById js/document id)
+            *index* 0]
+    (while (pos? (alength (.-childNodes *parent*)))    ;XXX patch
+      (.removeChild *parent* (.-firstChild *parent*))) ;XXX patch
+    (.appendChild *parent* (render widget)) ;TODO: figure out how to leverage put-element
+    ;;TODO process moved/removed nodes
+    ))
