ca1a2c64772c8164aa02f744259fad8d1ed71d33
Brandon Bloom <brandon@brandonbloom.name>
2015-07-26 23:08:14 -0700

    Bump vdom dep to workaround earlier clojars snafu

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "0.0-3269"]
                  [org.clojure/core.rrb-vector "0.0.11"]
-                 [bbloom.vdom "0.0.1"]]
+                 [bbloom.vdom "0.0.2"]]
 
   :plugins [[lein-cljsbuild "1.0.5"]
             [lein-figwheel "0.3.3"]]
